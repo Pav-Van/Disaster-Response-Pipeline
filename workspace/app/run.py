dfeb2a961d0cd783c73c44dc467324e344257935
Philip Van Deventer <phewvade@gmail.com>
2023-03-23 16:26:51 -0400

    Completed dashboard Code

--- workspace/app/run.py
+++ workspace/app/run.py
@@ -2,13 +2,14 @@ import json
 import plotly
 import pandas as pd
 import joblib
+import numpy as np
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+from plotly.graph_objs import Bar, Scatter, Heatmap
 from sqlalchemy import create_engine
 
 
@@ -52,12 +53,18 @@ model = joblib.load("../models/classifier.pkl")
 def index():
     
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
+
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
+
+    #selecting the sum of the last 36 columns, sorting them in descending order, and splicing the top 10. 
+    category_proportion = ((df.iloc[:,4:].sum())/df.shape[0]).sort_values(ascending=False)[:10]
+    category_names = list(category_proportion.index)
+
+    #creating correlation matrix for heatmap, dropped the first four columns
+    df_corr = df.iloc[:,4:].corr()
+
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
             'data': [
@@ -74,6 +81,39 @@ def index():
                 },
                 'xaxis': {
                     'title': "Genre"
+                },
+            }
+        },
+        {
+            'data': [
+                Heatmap(
+                    x=df_corr.columns,
+                    y=df_corr.index,
+                    z=np.array(df_corr)
+                )
+            ],
+
+            'layout': {
+                'title': 'Category Heatmap',
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_proportion
+                )
+            ],
+
+            'layout': {
+                'title': 'Top 10 Selected Categories',
+                'yaxis': {
+                    'title': "Proportion of Categories Selected"
+                },
+                'xaxis': {
+                    'title': {
+                        'text': "Category Name",
+                    }
                 }
             }
         }
