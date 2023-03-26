04929aa86b207099f53011ea58c9fbd913ebff20
Pav-Van <phewvade@gmail.com>
2023-03-26 05:35:53 -0400

    Reverting to Reran scripts

--- workspace/models/train_classifier.py
+++ workspace/models/train_classifier.py
@@ -1,10 +1,10 @@
 import sys
 import pandas as pd
 import numpy as np
+import scipy as sp
 import re
 from sqlalchemy import create_engine
 import joblib
-import random
 
 import nltk
 from nltk.tokenize import word_tokenize
@@ -17,7 +17,6 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report
 from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import NearestNeighbors
 
 nltk.download("punkt")
 nltk.download('stopwords')
@@ -143,89 +142,12 @@ def save_model(model, model_filepath):
     joblib.dump(model, model_filepath)
 
 
-def get_tail_label(df: pd.DataFrame, ql=[0.05, 1.]) -> list:
-    """
-    Find the underrepresented targets.
-    Underrepresented targets are those which are observed less than the median occurance.
-    Targets beyond a quantile limit are filtered.
-    """
-    irlbl = df.sum(axis=0)
-    irlbl = irlbl[(irlbl > irlbl.quantile(ql[0])) & ((irlbl < irlbl.quantile(ql[1])))]  # Filtering
-    irlbl = irlbl.max() / irlbl
-    threshold_irlbl = irlbl.median()
-    tail_label = irlbl[irlbl > threshold_irlbl].index.tolist()
-    return tail_label
-
-
-def get_minority_samples(X: pd.DataFrame, y: pd.DataFrame, ql=[0.05, 1.]):
-    """
-    return
-    X_sub: pandas.DataFrame, the feature vector minority dataframe
-    y_sub: pandas.DataFrame, the target vector minority dataframe
-    """
-    tail_labels = get_tail_label(y, ql=ql)
-    index = y[y[tail_labels].apply(lambda x: (x == 1).any(), axis=1)].index.tolist()
-    
-    X_sub = X[X.index.isin(index)].reset_index(drop = True)
-    y_sub = y[y.index.isin(index)].reset_index(drop = True)
-    return X_sub, y_sub
-
-
-def nearest_neighbour(X: pd.DataFrame, neigh) -> list:
-    """
-    Give index of 10 nearest neighbor of all the instance
-    
-    args
-    X: np.array, array whose nearest neighbor has to find
-    
-    return
-    indices: list of list, index of 5 NN of each element in X
-    """
-    nbs = NearestNeighbors(n_neighbors=neigh, metric='euclidean', algorithm='kd_tree').fit(X)
-    euclidean, indices = nbs.kneighbors(X)
-    return indices
-
-
-def MLSMOTE(X, y, n_sample, neigh=5):
-    """
-    Give the augmented data using MLSMOTE algorithm
-    
-    args
-    X: pandas.DataFrame, input vector DataFrame
-    y: pandas.DataFrame, feature vector dataframe
-    n_sample: int, number of newly generated sample
-    
-    return
-    new_X: pandas.DataFrame, augmented feature vector data
-    target: pandas.DataFrame, augmented target vector data
-    """
-    indices2 = nearest_neighbour(X, neigh=5)
-    n = len(indices2)
-    new_X = np.zeros((n_sample, X.shape[1]))
-    target = np.zeros((n_sample, y.shape[1]))
-    for i in range(n_sample):
-        reference = random.randint(0, n-1)
-        neighbor = random.choice(indices2[reference, 1:])
-        all_point = indices2[reference]
-        nn_df = y[y.index.isin(all_point)]
-        ser = nn_df.sum(axis = 0, skipna = True)
-        target[i] = np.array([1 if val > 0 else 0 for val in ser])
-        ratio = random.random()
-        gap = X.loc[reference,:] - X.loc[neighbor,:]
-        new_X[i] = np.array(X.loc[reference,:] + ratio * gap)
-    new_X = pd.DataFrame(new_X, columns=X.columns)
-    target = pd.DataFrame(target, columns=y.columns)
-    return new_X, target
-
-
 def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, y, category_names = load_data(database_filepath)
-        X_sub, y_sub = get_minority_samples(X, y)  # Getting minority samples of that datframe
-        X_res, y_res = MLSMOTE(X_sub, y_sub, 100, 5)  # Applying MLSMOTE to augment the dataframe
-        X_train, X_test, Y_train, Y_test = train_test_split(X_res, y_res, test_size=0.2)
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
         print('Building model...')
         model = build_model()
